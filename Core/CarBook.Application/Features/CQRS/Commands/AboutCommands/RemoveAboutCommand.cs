b46f83467156cb3113781991524fec7321d83156
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2025-04-07 15:50:46 +0300

    Add CQRS commands and handlers for About entities
    Updated project references and added command classes for creating, removing, and updating "About" entities. Implemented corresponding command handlers and query classes to facilitate CRUD operations. Organized code structure under appropriate namespaces and added necessary using directives.

---
+++ Core/CarBook.Application/Features/CQRS/Commands/AboutCommands/RemoveAboutCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.CQRS.Commands.AboutCommands
+{
+    public class RemoveAboutCommand
+    {
+        public RemoveAboutCommand(int aboutID)
+        {
+            Id = aboutID;
+        }
+
+        public int Id { get; set; }
+
+    }
+}
