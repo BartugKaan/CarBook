e642da473257f56212da12a786fc9c2b793c9af6
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2025-04-21 21:13:58 +0300

    Add CarBook.DTO project and update web components
    - Introduced `CarBook.DTO` project for data transfer objects.
    - Added `ResultAboutDto` and `ResultTestimonialDto` classes.
    - Updated project files to target .NET 8.0 and include necessary references.
    - Implemented view components to fetch and display data from the API.
    - Modified views to utilize new components and display dynamic content.
    - Added `TestimonialController` for handling API requests related to testimonials.
    - Translated various HTML sections in `Default.cshtml` to Turkish for localization.

---
+++ Frontends/CarBook.DTO/TestimonialDtos/ResultTestimonialDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.DTO.TestimonialDtos
+{
+    public class ResultTestimonialDto
+    {
+            public int testimonialID { get; set; }
+            public string name { get; set; }
+            public string title { get; set; }
+            public object comment { get; set; }
+            public string imageUrl { get; set; }
+    }
+}
