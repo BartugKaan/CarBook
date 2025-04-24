af1672ab17eec40a2e51667cdf76c2f86f9c3431
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2025-04-24 15:12:40 +0300

    Add author and blog management features
    This commit introduces a new feature set for managing authors and blogs in the CarBook application. It includes the implementation of command and query handlers using MediatR for CQRS, allowing for the creation, removal, and updating of authors and blogs.
    
    New command classes and their handlers have been added, along with query classes and result classes to retrieve author and blog data. The `Author` and `Blog` entities have been defined with appropriate properties and relationships.
    
    The database context has been updated to include `DbSet<Author>` and `DbSet<Blog>`, and migrations have been created to reflect these changes in the database schema. Additionally, the `IBlogRepository` interface and its implementation have been added for data access logic.
    
    Controllers for authors and blogs have been updated to provide HTTP endpoints for managing these entities, and the dependency injection container has been modified to register the new services.

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/BlogHandlers/GetLastThreeBlogsWithAuthorsQueryHandler.cs
@@ -0,0 +1,39 @@
+﻿using CarBook.Application.Features.CQRS.Results.CarResult;
+using CarBook.Application.Features.Mediator.Queries.BlogQueries;
+using CarBook.Application.Features.Mediator.Results.BlogResults;
+using CarBook.Application.Interfaces.BlogInterfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.Mediator.Handlers.BlogHandlers
+{
+    public class GetLastThreeBlogsWithAuthorsQueryHandler : 
+        IRequestHandler<GetLastThreeBlogsWithAuthorsQuery, List<GetLastThreeBlogsWithAuthorsQueryResult>>
+    {
+        private readonly IBlogRepository _repository;
+
+        public GetLastThreeBlogsWithAuthorsQueryHandler(IBlogRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<GetLastThreeBlogsWithAuthorsQueryResult>> Handle(GetLastThreeBlogsWithAuthorsQuery request, CancellationToken cancellationToken)
+        {
+            var values = _repository.GetLastThreeBlogsWithAuthors();
+            return values.Select(x => new GetLastThreeBlogsWithAuthorsQueryResult
+            {
+                AuthorID = x.AuthorID,
+                BlogID = x.BlogID,
+                CategoryId = x.CategoryId,
+                CoverImageUrl = x.CoverImageUrl,
+                CreatedDate = x.CreatedDate,
+                Title = x.Title,
+                AuthorName = x.Author.Name
+            }).ToList();
+        }
+    }
+}
--- Presentation/CarBook.WebApi/Program.cs
+++ Presentation/CarBook.WebApi/Program.cs
@@ -5,11 +5,13 @@ using CarBook.Application.Features.CQRS.Handlers.CarHandlers;
 using CarBook.Application.Features.CQRS.Handlers.CategoryHandlers;
 using CarBook.Application.Features.CQRS.Handlers.ContactHandlers;
 using CarBook.Application.Interfaces;
+using CarBook.Application.Interfaces.BlogInterfaces;
 using CarBook.Application.Interfaces.CarInterfaces;
 using CarBook.Application.Interfaces.CarRepositories;
 using CarBook.Application.Services;
 using CarBook.Persistance.Context;
 using CarBook.Persistance.Repositories;
+using CarBook.Persistance.Repositories.BlogRepositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +19,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddScoped<CarBookContext>();
 builder.Services.AddScoped(typeof(IRepository<>),typeof(Repository<>));
 builder.Services.AddScoped<ICarRepository, CarRepository>();
+builder.Services.AddScoped<IBlogRepository, BlogRepository>();
 
 
 builder.Services.AddScoped<GetBannerQueryHandler>();
